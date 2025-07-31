b8d548818b90dff0947dff9e618edf186e5d6907
pranjal verma <pranjalverma975@gmail.com>
2025-07-31 16:25:53 +0530

    ch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "@/components/StarBackground";
 export const Home =()=>{
-    return <div className="min-h-screen">
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
        {/*Theme Toggle */}
-
+<ThemeToggle/>
        {/*Background Effect*/}
-
+<StarBackground/>
        {/*Navbar*/}
 
        {/*Main Content*/}
