4af7840bcab3b3a829586aeedfa6757923113b4a
pranjal verma <pranjalverma975@gmail.com>
2025-07-31 13:34:33 +0530

    1st

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
 export const Home =()=>{
-    return <div>
-        Home
+    return <div className="min-h-screen">
+       {/*Theme Toggle */}
+
+       {/*Background Effect*/}
+
+       {/*Navbar*/}
+
+       {/*Main Content*/}
+
+       {/*Footer*/}
+
+       
     </div>
 }
