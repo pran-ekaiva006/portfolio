b958239b9cd9dd14cad66eab6bf4f9b404ca884e
pranjal kumar verma <pranjalkumarverma@pranjals-MacBook-Air.local>
2025-08-01 16:09:38 +0530

    iii

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import { Toaster } from "./components/ui/toaster";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 function App() {
   return(
   <>
-  <Toaster/>
   <BrowserRouter>
   <Routes>
     <Route index element={<Home/>}/>
