d29d9d0ab20490d50432fb024554249fb604d682
pranjal kumar verma <pranjalkumarverma@pranjals-MacBook-Air.local>
2025-08-01 16:06:30 +0530

    saveing

--- pranjal_portfolio/src/pages/Home.jsx
+++ pranjal_portfolio/src/pages/Home.jsx
@@ -1,5 +1,28 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "@/components/StarBackground";
+import { Navbar } from "../components/Navbar";
+import { Herosection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 export const Home =()=>{
-    return <div>
-        Home
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+       {/*Theme Toggle */}
+<ThemeToggle/>
+       {/*Background Effect*/}
+<StarBackground/>
+       {/*Navbar*/}
+<Navbar/>
+       {/*Main Content*/}
+<Herosection/>
+<AboutSection/>
+<SkillsSection/>
+<ProjectsSection/>
+<ContactSection/>
+       {/*Footer*/}
+       <Footer/>
+       
     </div>
 }
