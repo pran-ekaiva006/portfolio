ba8018ae8fffd06878a181bfe453edaada029b65
Pranjal Kumar Verma <pranjalverma975@gmail.com>
2026-05-31 01:05:47 +0530

    fix: render Toaster in App layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
+import { Toaster } from "./components/ui/toaster";
 function App() {
   return(
   <>
@@ -11,6 +12,7 @@ function App() {
 
     <Route path="*" element={<NotFound/>}/>
   </Routes>
+  <Toaster />
   </BrowserRouter>
   </>
   );
